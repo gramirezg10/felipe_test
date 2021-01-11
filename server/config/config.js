9e7ebf1fd650080da276f19bc50762de458c43f1
German Ramirez Gaviria <gramirezg10@gmail.com>
2021-01-11 18:03:26 -0500

    se quita la variable de la DB en el archivo de configuración

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,4 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.ENV = process.env.ENV || 'DEV';
 
 // DB
-if (process.env.ENV == 'DEV') process.env.URL_DB = 'mongodb://localhost:27017/bco_products'
-else process.env.URL_DB = 'mongodb+srv://admin:50HgvAimB63WXEdm@cluster0.4ynpz.mongodb.net/bco_products'
+process.env.URL_DB = process.env.URL_DB || 'mongodb://localhost:27017/bco_products'
