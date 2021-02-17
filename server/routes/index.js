a7c0e888b51d154b3c562f745cd8dc85b20ed71c
German Ramirez Gaviria <gramirezg10@gmail.com>
2021-02-17 10:27:16 -0500

    se implementa el get y el post básico de empleados

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(require('./products'));
+app.use(require('./employees'));
 // app.use(require('./smoketest'));
 
 module.exports = app;
